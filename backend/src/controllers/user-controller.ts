c6b5092a5d8d4ad880bcc7cbf7972438072c3967
Leonan Martins <leonanm04@gmail.com>
2023-08-18 14:46:55 -0300

    feat: create user controller

---
+++ backend/src/controllers/user-controller.ts
@@ -0,0 +1,19 @@
+import { CreateUserParams } from '@/services'
+import { userService } from '@/services'
+import { NextFunction, Request, Response } from 'express'
+import httpStatus from 'http-status'
+
+export async function postUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const data = req.body as CreateUserParams
+
+    try {
+        const { id, name, email } = await userService.create(data)
+        return res.status(httpStatus.CREATED).send({ id, name, email })
+    } catch (error) {
+        next(error)
+    }
+}
