0c64f89b8a732f31181680525a775f395ba22222
Leonan Martins <leonanm04@gmail.com>
2023-08-18 14:28:48 -0300

    feat: find user by email

--- backend/src/repositories/user-repository.ts
+++ backend/src/repositories/user-repository.ts
@@ -4,3 +4,11 @@ import { Prisma } from '@prisma/client'
 async function create(data: Prisma.UserCreateInput) {
     return await prisma.user.create({ data })
 }
+
+export const userRepository = {
+    create
+}
+
+async function findByEmail(email: string) {
+    return prisma.user.findUnique({ where: { email } })
+}
